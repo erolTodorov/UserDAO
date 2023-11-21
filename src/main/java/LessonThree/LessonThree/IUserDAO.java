5e159005a3018cf73a0e17a9047bc79b8d479040
erolTodorov <150246359+erolTodorov@users.noreply.github.com>
2023-11-21 16:31:31 +0200

    Update IUserDAO.java

--- src/main/java/LessonThree/LessonThree/IUserDAO.java
+++ src/main/java/LessonThree/LessonThree/IUserDAO.java
@@ -1,5 +1,16 @@
 package LessonThree.LessonThree;
 
-public class IUserDAO {
+import java.util.List;
 
+interface IUserDAO {
+	
+    void insert(User user);
+
+    void updateUserFirstName(int id, String newName);
+
+    User getUserById(int id);
+
+    List<User> getAllUsers();
+
+    void delete(User user);
 }

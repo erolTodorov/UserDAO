92b31295794961ebf47fa88a81ef1ce190a88c06
erolTodorov <150246359+erolTodorov@users.noreply.github.com>
2023-11-21 16:33:05 +0200

    Update UserDAO.java

--- src/main/java/LessonThree/LessonThree/UserDAO.java
+++ src/main/java/LessonThree/LessonThree/UserDAO.java
@@ -1,5 +1,51 @@
 package LessonThree.LessonThree;
 
-public class UserDAO {
+import java.util.List;
 
+class UserDAO implements IUserDAO {
+    private List<User> userList; 
+
+    
+    public UserDAO(List<User> userList) {
+        this.userList = userList;
+    }
+
+    @Override
+    public void insert(User user) {
+        userList.add(user);
+    }
+
+    @Override
+    public void updateUserFirstName(int id, String newName) {
+        for (User user : userList) {
+            if (user.getId() == id) {
+                user.setFirstName(newName);
+                return;
+            }
+        }
+        
+        System.out.println("User with ID " + id + " not found.");
+    }
+
+    @Override
+    public User getUserById(int id) {
+        for (User user : userList) {
+            if (user.getId() == id) {
+                return user;
+            }
+        }
+        // Handle the case where the user with the given id is not found
+        System.out.println("User with ID " + id + " not found.");
+        return null;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userList;
+    }
+
+    @Override
+    public void delete(User user) {
+        userList.remove(user);
+    }
 }

a93bb1f1abda52932aef64d85d3978b6841a8000
erolTodorov <150246359+erolTodorov@users.noreply.github.com>
2023-11-21 16:32:09 +0200

    Update User.java

--- src/main/java/LessonThree/LessonThree/User.java
+++ src/main/java/LessonThree/LessonThree/User.java
@@ -1,14 +1,56 @@
 package LessonThree.LessonThree;
+
+//import java.util.List;
+
 class User {
     private int id;
     private String username;
     private String firstName;
     private String lastName;
 
-    // Constructor, getters, and setters
-    // ...
+    
 
-    @Override
+    public User(int id, String username, String firstName, String lastName) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	@Override
     public String toString() {
         return "User{" +
                 "id=" + id +
